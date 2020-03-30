19a24a3dc60fee89657725e2163a16d0e3de79de
Ian Fried <ifried00@stuy.edu>
2020-03-30 05:19:52 -0400

    final commit for now

--- image.py
+++ image.py
@@ -15,7 +15,7 @@ def matrix_mult( m1, m2 ):
     while i < len(m2):
         if m2[i]:
             m2[i] = temp[i]
-        else:
+        elif temp[i]:
             m2.append(temp[i])
         i += 1
     return m2
@@ -102,35 +102,52 @@ def circle_point(x, y, theta, r):
     return [xn, yn]
 
 
+# Vector related function
+def dot_product(a, b):
+    """ Return the dot product of 2 vectors. """
+    return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2])
+def normal(m, i):
+    """ Returns the normal of a polygon in a given matrix. """
+    a = [m[0][i + 1] - m[0][i],
+	 m[1][i + 1] - m[1][i],
+	 m[2][i + 1] - m[2][i]]
+    b = [m[0][i + 2] - m[0][i],
+	 m[1][i + 2] - m[1][i],
+	 m[2][i + 2] - m[2][i]]
+    n = [(a[1] * b[2]) - (a[2] * b[1]),
+	 (a[2] * b[0]) - (a[0] * b[2]),
+	 (a[0] * b[1]) - (a[1] * b[0])];
+    return n
+
 # Point generation for useful 3d shapes. And toruses...
+## Useful globals
+### 3d shape resolution
+S_RES = 30 # Resolution for sphere
+T_RES = 20 # Resolution for torus
+
 def generate_sphere(x, y, z, r):
     """ Generates the points on a sphere, stores them in a 3xn array, and returns them. """
-    m = new_matrix(3, 1)
-    t_inc = PI /  30
-    theta = 0
-    while theta < 2 * PI:
+    m = new_matrix(3, 0)
+    for t in range(2 * S_RES):
+        theta = t * PI / S_RES
         phi = 0
-        while phi < PI:
+        for p in range(S_RES):
+            phi = p * PI / (S_RES - 1)
             m[0].append(x + (r * cos(theta) * sin(phi)))
             m[1].append(y + (r * sin(theta) * sin(phi)))
             m[2].append(z + (r * cos(phi)))
-            phi += t_inc
-            theta += t_inc
     return m
 
 def generate_torus(x, y, z, r, R):
     """ Generates the points on a torus, stores them in a 3xn array, and returns them. """
-    m = new_matrix(3, 1)
-    t_inc = PI /  30
-    theta = 0
-    while theta < 2 * PI:
-        phi = 0
-        while phi < 2 * PI:
+    m = new_matrix(3, 0)
+    for t in range(2 * T_RES):
+        theta = t * PI / T_RES
+        for p in range(2 * T_RES):
+            phi = p * PI / T_RES
             m[0].append(x + (cos(theta) * (r * cos(phi) + R)))
             m[1].append(y + (r * sin(phi)))
             m[2].append(z - (sin(theta) * (r * cos(phi) + R)))
-            phi += t_inc
-            theta += t_inc
     return m
 
 
@@ -138,10 +155,12 @@ class Picture:
     """ A class that represents a canvas, upon which artists can create beautiful works."""
     def __init__(self, n, w, h):
         self.name = n
+        self.view_vector = [0, 0, 1]
         self.width, self.height = (w, h)
         self.pixels = [[[0, 0, 0] for i in range(self.width)] for j in range(self.height)]
         self.four_identity = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
         self.edge_matrix = [[], [], [], []]
+        self.triangle_matrix = [[], [], [], []]
         self.transformation_matrix = ident(new_matrix())
 
     def plot(self, x, y, color):
@@ -235,18 +254,23 @@ class Picture:
     def display_edge_matrix(self):
         for i in self.edge_matrix:
             print(i)
-    def add_3d_point(self, x, y, z):
-        """ Add a single point to the edge matrix. """
-        self.edge_matrix[0].append(x)
-        self.edge_matrix[1].append(y)
-        self.edge_matrix[2].append(z)
-        self.edge_matrix[3].append(1)
-
+    def add_3d_point(self, x, y, z, m=None):
+        """ Add a single point to the edge matrix by default or triangle matrix if specified. """
+        if not m:
+            m = self.edge_matrix
+        m[0].append(x)
+        m[1].append(y)
+        m[2].append(z)
+        m[3].append(1)
     def add_edge(self, x0, y0, z0, x1, y1, z1):
         """ Add an edge to the edge matrix. """
         self.add_3d_point(x0,y0,z0)
         self.add_3d_point(x1,y1,z1)
-
+    def add_polygon(self, x0, y0, z0, x1, y1, z1, x2, y2, z2):
+        """ Add a polygon to the edge matrix. """
+        self.add_3d_point(x0,y0,z0, m=self.triangle_matrix)
+        self.add_3d_point(x1,y1,z1, m=self.triangle_matrix)
+        self.add_3d_point(x2,y2,z2, m=self.triangle_matrix)
     def add_circle(self, cx, cy, cz, r, step ):
         """ Add a circle to the edge matrix. """
         centx = cx
@@ -282,35 +306,98 @@ class Picture:
                 t += step
 
     def add_box(self, x, y, z, width, height, depth):
-        """ Add a box to the edge matrix. Rectangular Prism is such an ugly word, using box means instead means I take up less space. """
-        # Front face
-        self.add_edge(x, y, z, x + width, y, z)
-        self.add_edge(x, y, z, x, y - height, z)
-        self.add_edge(x + width, y - height, z, x + width, y, z)
-        self.add_edge(x + width, y - height, z, x, y - height, z)
-        # Connect the front and back faces
-        self.add_edge(x, y, z, x, y, z - depth)
-        self.add_edge(x + width, y, z, x + width, y, z - depth)
-        self.add_edge(x, y - height, z, x, y - height, z - depth)
-        self.add_edge(x + width, y - height, z, x + width, y - height, z - depth)
-        # Back face
-        self.add_edge(x, y, z - depth, x + width, y, z - depth)
-        self.add_edge(x, y, z - depth, x, y - height, z - depth)
-        self.add_edge(x + width, y - height, z - depth, x + width, y, z - depth)
-        self.add_edge(x + width, y - height, z - depth, x, y - height, z - depth)
+        """
+        Add a box to the edge matrix. Rectangular Prism is such an ugly word, using box means instead means I take up less space.
 
+        points (for reference):
+          4_____7
+         /:    /|
+        0_____3 |
+        | 5---|-6
+        |/    |/
+        1_____2
+        """
+        points = [[x, y, z], [x, y - height, z], [x + width, y - height, z], [x + width, y, z],
+		  [x, y, z - depth], [x, y - height, z - depth], [x + width, y - height, z - depth], [x + width, y, z - depth]]
+        # add_polygon returns null, so the lambda makes a rectangular face by returning an or statement of the add_polygon calls on the 2 triangles that make it up. Simplifies code.
+        add_side = lambda screen, a, b, c, d : (
+            screen.add_polygon(points[a][0], points[a][1], points[a][2],
+                               points[b][0], points[b][1], points[b][2],
+                               points[c][0], points[c][1], points[c][2])
+            or
+            screen.add_polygon(points[c][0], points[c][1], points[c][2],
+                               points[d][0], points[d][1], points[d][2],
+                               points[a][0], points[a][1], points[a][2]))
+        add_side(self, 0, 1, 2, 3);
+        add_side(self, 4, 0, 3, 7);
+        add_side(self, 3, 2, 6, 7);
+        add_side(self, 2, 1, 5, 6);
+        add_side(self, 4, 5, 1, 0);
+        add_side(self, 7, 6, 5, 4);
     def add_sphere(self, x, y, z, r):
-        """ Add a sphere to the edge matrix. """
+        """
+        Add a sphere to the polygon matrix.
+           a---i      |  a - i            |   a   i
+           A          |  | X |            |     V
+         q - t        |  q - t            | q---t
+        [i, res - 1)  |  [i + 1, res - 2) | [res - 1]
+
+        res = 5:
+            10---5/0          TQA
+          11    6   1    ITA, TQA
+        12     7     2   ITA, TQA
+          13    8   3    ITA,
+            14---9/4
+
+        """
+
+
+
         m = generate_sphere(x, y, z, r)
         for i in range(len(m[0])):
-            self.add_edge(m[0][i], m[1][i], m[2][i],
-                          m[0][i] + 1, m[1][i] + 1, m[2][i] + 1)
+            a = (i + 1)
+            t = (i + S_RES) % (2 * S_RES * S_RES)
+            q = (a + S_RES) % (2 * S_RES * S_RES)
+            if (i + 1) % S_RES ==  0:
+                continue
+            if i % S_RES > 0:
+                self.add_polygon(m[0][i], m[1][i], m[2][i],
+                                 m[0][t], m[1][t], m[2][t],
+                                 m[0][a], m[1][a], m[2][a])
+            if (i + 2) % S_RES > 0:
+                self.add_polygon(m[0][t], m[1][t], m[2][t],
+                                 m[0][q], m[1][q], m[2][q],
+                                 m[0][a], m[1][a], m[2][a])
     def add_torus(self, x, y, z, r, R):
-        """ Add a torus to the edge matrix. R is the radius of the torus, r is the radius of the circle. """
+        """
+        Add a torus to the edge matrix. R is the radius of the torus, r is the radius of the circle.
+
+          a - i
+          | X |
+          q - t
+
+
+        res = 4:
+           8---------0
+         15_9-------7_1
+        14___10----6___2
+         13_11------5_3
+          12---------4
+        """
+
         m = generate_torus(x, y, z, r, R)
         for i in range(len(m[0])):
-            self.add_edge(m[0][i], m[1][i], m[2][i],
-                          m[0][i] + 1, m[1][i] + 1, m[2][i] + 1)
+             a = i + 1
+             a = a if (a % (2 * T_RES)) else a - 2 * T_RES;
+             t = (i + 2 * T_RES) % (4 * T_RES * T_RES)
+             q = (a + 2 * T_RES) % (4 * T_RES * T_RES)
+             self.add_polygon(m[0][i], m[1][i], m[2][i],
+                              m[0][t], m[1][t], m[2][t],
+                              m[0][a], m[1][a], m[2][a])
+
+             self.add_polygon(m[0][t], m[1][t], m[2][t],
+                              m[0][q], m[1][q], m[2][q],
+                              m[0][a], m[1][a], m[2][a])
     def draw_lines(self):
         """ Draw the edges in the edge matrix. """
         matrix = self.edge_matrix
@@ -326,13 +413,31 @@ class Picture:
                        [255, 255, 255])
             point+= 2
 
+    def draw_polygons(self):
+        matrix = self.triangle_matrix
+        if len(matrix) < 3:
+            print('Need at least 3 points to draw')
+            return
+        point = 0
+        while point < len(matrix[0]) - 1:
+            if dot_product(normal(matrix, point), self.view_vector) >= 0:
+                self.draw_line( int(matrix[0][point]), int(matrix[1][point]),
+                                int(matrix[0][point+1]),int(matrix[1][point+1]),
+                                [255, 255, 255])
+                self.draw_line( int(matrix[0][point]), int(matrix[1][point]),
+                                int(matrix[0][point+2]),int(matrix[1][point+2]),
+                                [255, 255, 255])
+                self.draw_line( int(matrix[0][point+2]), int(matrix[1][point+2]),
+                                int(matrix[0][point+1]),int(matrix[1][point+1]),
+                                [255, 255, 255])
+            point += 3
     def clear_screen(self):
         """ Clear the pixels in the screen. """
         self.pixels = [[[0, 0, 0] for i in range(self.width)] for j in range(self.height)]
     def clear_edge_matrix(self):
         """ Clear the edge matrix. Useful for removing shapes you no longer want anything to do with. """
         self.edge_matrix = [[], [], [], []]
-
+        self.triangle_matrix = [[], [], [], []]
     def pixels_to_ascii(self):
         """ Returns the picture in ppm string format. """
         s = ""
@@ -386,6 +491,7 @@ def parse_file( fname, transform, screen, color ):
                       float(args[6]), float(args[7]), 0.005, "hermite")
 
         elif line == 'box':
+            pass
             screen.add_box(
                 float(args[0]), float(args[1]), float(args[2]),
                 float(args[3]), float(args[4]), float(args[5]) )
@@ -419,12 +525,14 @@ def parse_file( fname, transform, screen, color ):
 
         elif line == 'apply':
             matrix_mult(transform, screen.edge_matrix )
+            matrix_mult(transform, screen.triangle_matrix )
 
         elif line == 'clear':
-            screen.edge_matrix = [[], [], [], []]
+            screen.clear_edge_matrix()
         elif line == 'display' or line == 'save':
             screen.clear_screen()
             screen.draw_lines()
+            screen.draw_polygons()
             screen.save()
             if line == 'display':
                 os.system('display *.ppm')
